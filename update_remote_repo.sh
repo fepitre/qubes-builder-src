a1fb4d4edecae1009453cf97ad304790f4d9cc39
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-07-20 12:10:04 +0200

    WIP: improve with respect to Marek's comments

--- update_remote_repo.sh
+++ update_remote_repo.sh
@@ -3,11 +3,20 @@
 set -e
 [[ "$DEBUG" == 1 ]] && set -x
 
+RELEASE="$1"
+
+if [ -z "$UPDATE_REPO" ]; then
+    echo "You need to specify destination repo in UPDATE_REPO variable"
+    exit 1
+fi
+
 [ -z "$HOST" ] && HOST=distfiles.qubes-os.org
-[ -z "$HOST_BASEDIR" ] && HOST_BASEDIR=/pub/qubes/repo/sources
-[ -z "$RELEASE" ] && RELEASE=current-release
+[ -z "$HOST_BASEDIR" ] && HOST_BASEDIR=/pub/qubes/repo/src
+
+UPDATE_REPO=${UPDATE_REPO//current*\/dom0\/src}
 
 if [ "$AUTOMATIC_UPLOAD" = 1 ]; then
-    echo "Syncing sources for $RELEASE..."
-    rsync $DRY --partial --progress --hard-links --exclude repodata -air "${BUILDER_SRC_DIR}/r$RELEASE/sources" "$HOST:$HOST_BASEDIR/$RELEASE/"
+    echo "Syncing sources..."
+    rsync $DRY --partial --progress --hard-links --exclude repodata -air "${UPDATE_REPO}/common" "$HOST:$HOST_BASEDIR/"
+    rsync $DRY --partial --progress --hard-links --exclude repodata -air "${UPDATE_REPO}/$RELEASE" "$HOST:$HOST_BASEDIR/"
 fi
